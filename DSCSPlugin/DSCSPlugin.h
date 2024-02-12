77cc4ce6bea8299537f79b83e5670be0fd1bcbae
Mathis Serrieres Maniecki <mathis.sema@gmail.com>
2024-02-12 10:49:38 +0100

    refactor: Remove old functions

--- DSCSPlugin/DSCSPlugin.h
+++ DSCSPlugin/DSCSPlugin.h
@@ -2,11 +2,6 @@
 #pragma comment( lib, "pluginsdk.lib" )
 
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include <sio_client.h>
-#include <fstream>
-#include <nlohmann/json.hpp>
-
-using json = nlohmann::json;
 
 class DSCSPlugin : public BakkesMod::Plugin::BakkesModPlugin
 {
@@ -14,27 +9,7 @@ public:
 	virtual void onLoad();
 	virtual void onUnload();
 
-	void JoinSpectator();
-	void SetSpectatorUI(int sleep);
-	void RemoveStatGraph();
-	void FetchStats();
-	void FetchPlayers();
-	void SetReplayAutoSave(bool status);
-	void ReadyUp();
-
 private:
-	sio::client socket;
-	std::time_t match_started_at = std::time(0);
-	std::time_t match_ended_at = std::time(0);
-	bool matchStatus = false;
-	bool playbackStatus = false;
-	bool highlightStatus = false;
-
-	bool IsGameValid();
-	void UpdateMatchStatus(bool status);
-	void UpdatePlaybackStatus(bool status);
-	void UpdateHighlightStatus(bool status);
-	void ResetStatus();
 	void Log(std::string message);
 };
 
