ba18237766d1535affe1a5d99176c4f8c2adc601
Minarox <contact@minarox.fr>
2024-02-13 20:11:33 +0100

    feat: Add IXWebSocket package

--- DSCSPlugin/DSCSPlugin.cpp
+++ DSCSPlugin/DSCSPlugin.cpp
@@ -8,17 +8,51 @@ BAKKESMOD_PLUGIN(DSCSPlugin, "DawaEsport Championship Plugin", "1.0", PERMISSION
 */
 void DSCSPlugin::onLoad()
 {
+	ix::initNetSystem();
+	this->LoadWebSocket();
+
 	this->Log("--- DSCSPlugin loaded ---");
 }
 
 void DSCSPlugin::onUnload()
 {
+	webSocket.stop();
+	ix::uninitNetSystem();
 	this->Log("--- DSCSPlugin unloaded ---");
 }
 
 /*
 	--- Private ---
 */
+void DSCSPlugin::LoadWebSocket()
+{
+	webSocket.setUrl("ws://localhost:9001/game");
+
+	webSocket.setPingInterval(1);
+	webSocket.start();
+
+	webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg)
+		{
+			switch (msg->type)
+			{
+			case ix::WebSocketMessageType::Open:
+				this->Log("WebSocket connected");
+				webSocket.send("hello world");
+				break;
+			case ix::WebSocketMessageType::Message:
+				this->Log("Received message: " + msg->str);
+				break;
+			case ix::WebSocketMessageType::Error:
+				this->Log("Error: " + msg->errorInfo.reason);
+				break;
+			case ix::WebSocketMessageType::Close:
+				this->Log("WebSocket closed");
+				break;
+			}
+		}
+	);
+}
+
 void DSCSPlugin::Log(std::string message)
 {
 	cvarManager->log(message);
--- DSCSPlugin/DSCSPlugin.h
+++ DSCSPlugin/DSCSPlugin.h
@@ -1,7 +1,11 @@
 #pragma once
-#pragma comment( lib, "pluginsdk.lib" )
+#pragma comment (lib, "pluginsdk.lib")
+#pragma comment (lib, "ws2_32.lib")
+#pragma comment (lib, "crypt32.lib")
 
 #include "bakkesmod/plugin/bakkesmodplugin.h"
+#include <ixwebsocket/IXNetSystem.h>
+#include <ixwebsocket/IXWebSocket.h>
 
 class DSCSPlugin : public BakkesMod::Plugin::BakkesModPlugin
 {
@@ -10,6 +14,8 @@ public:
 	virtual void onUnload();
 
 private:
+	ix::WebSocket webSocket;
+
+	void LoadWebSocket();
 	void Log(std::string message);
 };
-
