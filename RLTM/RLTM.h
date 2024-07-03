46bf7e37f195cd7b89cabff483f8e351b726ed15
Minarox <contact@minarox.fr>
2024-07-03 10:33:40 +0200

    feat: Import required packages

--- RLTM/RLTM.h
+++ RLTM/RLTM.h
@@ -1,8 +1,14 @@
 #pragma once
+#pragma comment (lib, "pluginsdk.lib")
+#pragma comment (lib, "ws2_32.lib")
+#pragma comment (lib, "crypt32.lib")
 
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 #include "bakkesmod/plugin/pluginwindow.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
+#include <ixwebsocket/IXNetSystem.h>
+#include <ixwebsocket/IXWebSocket.h>
+#include <nlohmann/json.hpp>
 
 #include "version.h"
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
