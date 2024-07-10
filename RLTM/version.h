47a8ffed441817181d96346e3b02667785ed962c
Minarox <contact@minarox.fr>
2024-07-11 01:35:35 +0200

    fix: isPaused state

--- RLTM/version.h
+++ RLTM/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 309
+#define VERSION_BUILD 322
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
