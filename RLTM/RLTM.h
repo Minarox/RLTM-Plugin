bd542a6248f71cf89fd6bea0a8cf3f185c2bce22
Mathis Serrieres Maniecki <mathis.sema@gmail.com>
2024-10-09 17:53:59 +0200

    refactor: Fetch players when game is playing

--- RLTM/RLTM.h
+++ RLTM/RLTM.h
@@ -74,9 +74,9 @@ class RLTM: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugi
 	json entitiesData = json::object();
 	ServerWrapper GetServerWrapper();
 	void SetReplayState(bool state, string caller);
-	void GetPlayersData(string caller);
 	void GetMatchData(string caller);
 	array<int, 2> GetScore(ServerWrapper server);
+	void GetPlayersData(ServerWrapper server);
 	void GetStatisticsData(ServerWrapper server);
 	void GetPlayerStatData(ServerWrapper _server, void* params);
 	void GetEntitiesData();
